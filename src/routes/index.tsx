337059217c29f5c3852aaad388c52227a8bde602
Juan Marín <juanpmarin@users.noreply.github.com>
2023-09-13 16:30:12 -0500

    feat: basic form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,56 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import {
+  formAction$,
+  useForm,
+  zodForm$,
+  type InitialValues,
+} from "@modular-forms/qwik";
+import { z } from "zod";
+
+export const HelloSchema = z.object({
+  name: z.string().min(3),
+});
+
+type HelloForm = z.input<typeof HelloSchema>;
+
+export const useFormLoader = routeLoader$<InitialValues<HelloForm>>(() => {
+  return {
+    name: "",
+  };
+});
+
+export const useFormAction = formAction$<HelloForm>((values, { redirect }) => {
+  console.log("VALUES", values);
+  console.log("THIS IS SERVER ONLY CODE");
+
+  throw redirect(302, "/success");
+}, zodForm$(HelloSchema));
 
 export default component$(() => {
+  const [, { Form, Field }] = useForm<HelloForm>({
+    loader: useFormLoader(),
+    validate: zodForm$(HelloSchema),
+    action: useFormAction(),
+  });
+
   return (
     <main>
       <h1>Submit this form 😁</h1>
+      <div>
+        <Form>
+          <Field name="name">
+            {(field, props) => (
+              <>
+                <label for="name">What's your name</label>
+                <input {...props} id="name" placeholder="John Doe" />
+                {field.error && <small>{field.error}</small>}
+              </>
+            )}
+          </Field>
+          <button type="submit">Submit</button>
+        </Form>
+      </div>
     </main>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
---
+++ src/routes/success/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <main>
+      <h1>Thank you! 🤩</h1>
+      <div>
+      </div>
+    </main>
+  );
+});
