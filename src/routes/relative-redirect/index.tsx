9926dc6b457f5cdc5dafc839aed33f89904d37e3
Juan Marín <juanpmarin@users.noreply.github.com>
2023-09-20 11:53:32 -0500

    feat: modular forms relative redirect

---
+++ src/routes/relative-redirect/index.tsx
@@ -0,0 +1,54 @@
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import {
+  formAction$,
+  useForm,
+  zodForm$,
+  type InitialValues,
+} from "@modular-forms/qwik";
+import { z } from "zod";
+
+export const HelloSchema = z.object({
+  name: z.string().min(3),
+});
+
+type HelloForm = z.input<typeof HelloSchema>;
+
+export const useFormLoader = routeLoader$<InitialValues<HelloForm>>(() => {
+  return {
+    name: "",
+  };
+});
+
+export const useFormAction = formAction$<HelloForm>((values, { redirect }) => {
+  // throw redirect(302, "/success"); // This works
+  throw redirect(302, "../success"); // This does not work
+}, zodForm$(HelloSchema));
+
+export default component$(() => {
+  const [, { Form, Field }] = useForm<HelloForm>({
+    loader: useFormLoader(),
+    validate: zodForm$(HelloSchema),
+    action: useFormAction(),
+  });
+
+  return (
+    <main>
+      <h1>Submit this form 😁</h1>
+      <div>
+        <Form>
+          <Field name="name">
+            {(field, props) => (
+              <>
+                <label for="name">What's your name</label>
+                <input {...props} id="name" placeholder="John Doe" />
+                {field.error && <small>{field.error}</small>}
+              </>
+            )}
+          </Field>
+          <button type="submit">Submit</button>
+        </Form>
+      </div>
+    </main>
+  );
+});
